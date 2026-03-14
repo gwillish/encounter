050eaf8d882d8be0a756ebea13357ecd0a4d4b79
Fritz <gwillish55@gmail.com>
2026-03-14 14:40:11 -0700

    Initial Commit

---
+++ Encounter/EncounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncounterApp.swift
+//  Encounter
+//
+//  Created by Joe Heck on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct EncounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EncounterTests/EncounterTests.swift
@@ -0,0 +1,16 @@
+//
+//  EncounterTests.swift
+//  EncounterTests
+//
+//  Created by Joe Heck on 3/14/26.
+//
+
+import Testing
+
+struct EncounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
